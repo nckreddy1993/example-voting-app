25c9b5dd17d76b9080082d9ae287adbe2085ba87
nckreddy1993 <153912763+nckreddy1993@users.noreply.github.com>
2024-02-08 23:07:30 +0530

    Update app.js

--- result/views/app.js
+++ result/views/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('CSKvsRCB', []);
+var app = angular.module('JaganvsPSPK', []);
 var socket = io.connect({transports:['polling']});
 
 var bg1 = document.getElementById('background-stats-1');
